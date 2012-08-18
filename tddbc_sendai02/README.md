cb0d6be35a075e56c19253a252b228781d15daca
thatblue <ancientblue@gmail.com>
2012-08-18 17:45:47 +0900

    ステップ2まで

---
+++ tddbc_sendai02/README.md
@@ -0,0 +1,7 @@
+tddbc_sendai02
+==============
+
+TDDBC仙台02の課題復習用
+
+課題：http://devtesting.jp/tddbc/?TDDBC大阪2.0%2F課題
+用語集：http://devtesting.jp/tddbc/?TDDBC仙台02%2F課題用語集
---
+++ tddbc_sendai02/test/com/example/thatblue/tddbc/VendingMachineTest.java
@@ -0,0 +1,54 @@
+package com.example.thatblue.tddbc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VendingMachineTest {
+
+    private VendingMachine vendingMachine = null; 
+    
+    @Before
+    public void before() {
+        vendingMachine = new VendingMachine();
+    }
+    
+    @Test
+    public void 初期状態で合計金額が0円であること() {
+        assertThat(vendingMachine.getTotalAmount(), is(0));
+    }
+
+    @Test
+    public void お金が投入できること() throws Exception {
+        vendingMachine.insert(10);
+        vendingMachine.insert(50);
+        vendingMachine.insert(100);
+        vendingMachine.insert(500);
+        vendingMachine.insert(1000);
+        assertThat(vendingMachine.getTotalAmount(), is(1660));
+    }
+    
+    @Test
+    public void 投入した金額を払い戻しできること() throws Exception {
+        vendingMachine.insert(100);
+        vendingMachine.insert(100);
+        vendingMachine.insert(10);
+        assertThat(vendingMachine.refund(), is(Arrays.asList(100,100,10)));
+        assertThat(vendingMachine.getTotalAmount(), is(0));
+    }
+    
+    @Test
+    public void 規定外のお金が投入された場合に返却されること() throws Exception {
+        try {
+            vendingMachine.insert(1);
+            fail("おかしな金額を入れているので、例外が起きないとダメ");
+        } catch (IllegalArgumentException e) {
+            assertThat(vendingMachine.getTotalAmount(),is(0));
+        }
+    }
+    
+    
+}
